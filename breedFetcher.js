93ffc179f2470f1a897a0a838cb40309be512e43
HermSidhu <h-sidhu@hotmail.com>
2019-10-24 20:38:20 +0000

    refactored and tested

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,17 @@
 const request = require('request');
-request(`https://api.thecatapi.com/v1/breeds/search?q=${process.argv[2]}`, (error, response, body) => {
-  if (error) console.log('error:', error); // console.log('error:', error); // Print the error if one occurred
-  else {
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-      return console.log("Are you sure you spelled the cat breed correctly?");
-    } else {
-      console.log(data[0].description);
+
+const fetchBreedDescription = function(breedName, callback) {
+  request.get(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) callback(error, null); // console.log('error:', error); // Print the error if one occurred
+    else {
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        callback(null, "Are you sure you spelled the cat breed correctly?");
+      } else {
+        callback(null, data[0].description);
+      }
     }
-  }
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };
